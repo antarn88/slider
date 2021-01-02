3c8711b946f2b35ded6f7897fac24d6aa2b38d89
Arnold <antaloczi.arnold@gmail.com>
2021-01-02 21:35:00 +0100

    Initial commit

---
+++ assets/js/domElements.js
@@ -0,0 +1,8 @@
+"use strict";
+
+export const slider = document.querySelector(".slider__photo-container");
+export const photoCounters = slider.querySelector(".slider__photo-counter");
+export const previousButton = slider.querySelector(".slider__previous-button");
+export const nextButton = slider.querySelector(".slider__next-button");
+export const photoTitle = slider.querySelector(".slider__photo-title");
+export const photoPagination = document.querySelectorAll(".slider__pagination span");
---
+++ assets/js/photoInfos.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const photoInfos = [
+    {
+        path: "../assets/img/1.jpg",
+        title: "Stars at night",
+    },
+    {
+        path: "../assets/img/2.jpg",
+        title: "Green grass and forest",
+    },
+    {
+        path: "../assets/img/3.jpg",
+        title: "Mountains",
+    },
+    {
+        path: "../assets/img/4.jpg",
+        title: "Hills",
+    },
+];
+
+export default photoInfos;
